9890a00e1efffb9e076f38bf1c2f6595bfec5e3a
Bora <bora@Bora-MacBook-Air.local>
2023-04-11 15:50:10 +0300

    update GlobalExceptionHandler

--- WeatherApiService/src/main/java/com/karacatech/weatherforecast/GlobalExceptionHandler.java
+++ WeatherApiService/src/main/java/com/karacatech/weatherforecast/GlobalExceptionHandler.java
@@ -3,16 +3,25 @@ package com.karacatech.weatherforecast;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.List;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
@@ -29,5 +38,22 @@ public class GlobalExceptionHandler {
         LOGGER.error("Error occurred", ex);
 
         return error;
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ErrorDTO error = new ErrorDTO();
+        error.setTimestamp(new Date());
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setPath(((ServletWebRequest) request).getRequest().getServletPath());
+
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        fieldErrors.forEach(fieldError -> {
+            error.addError(fieldError.getField() + " " + fieldError.getDefaultMessage());
+        });
+
+        return new ResponseEntity<>(error, headers, HttpStatus.BAD_REQUEST);
     }
 }
