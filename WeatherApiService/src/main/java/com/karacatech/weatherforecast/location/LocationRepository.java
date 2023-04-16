faaeddb8cd70627c75f9ce6bf0d273f8ba23b524
Bora <bora@Bora-MacBook-Air.local>
2023-04-16 23:45:49 +0300

    small fix in interface methods

--- WeatherApiService/src/main/java/com/karacatech/weatherforecast/location/LocationRepository.java
+++ WeatherApiService/src/main/java/com/karacatech/weatherforecast/location/LocationRepository.java
@@ -11,12 +11,12 @@ import java.util.List;
 public interface LocationRepository extends CrudRepository<Location, String> {
 
     @Query("SELECT l FROM Location l WHERE l.trashed = false")
-    public List<Location> findUntrashed();
+     List<Location> findUntrashed();
 
     @Query("SELECT l FROM Location l WHERE l.trashed = false AND l.code = ?1")
-    public Location findByCode(String code);
+     Location findByCode(String code);
 
     @Modifying
     @Query("UPDATE Location  SET trashed = true WHERE code = ?1")
-    public void trashByCode(String code);
+     void trashByCode(String code);
 }
