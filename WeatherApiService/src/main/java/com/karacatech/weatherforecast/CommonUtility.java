71f02589c3d6fdcf1f83e96090d0218232eb989f
Bora <bora@Bora-MacBook-Air.local>
2023-04-17 16:50:34 +0300

    implement RealtimeWeatherController class

---
+++ WeatherApiService/src/main/java/com/karacatech/weatherforecast/CommonUtility.java
@@ -0,0 +1,21 @@
+package com.karacatech.weatherforecast;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CommonUtility {
+    private static Logger LOGGER = LoggerFactory.getLogger(CommonUtility.class);
+
+    public static String getIPAddress(HttpServletRequest request){
+        String ipAddress = request.getHeader("X-FORWARDED-FOR");
+
+        if (ipAddress == null || ipAddress.isEmpty()) {
+            ipAddress = request.getRemoteAddr();
+        }
+
+        LOGGER.info("IP Address: {}", ipAddress);
+
+        return ipAddress;
+    }
+}
---
+++ WeatherApiService/src/main/java/com/karacatech/weatherforecast/realtime/RealtimeWeatherApiController.java
@@ -0,0 +1,53 @@
+package com.karacatech.weatherforecast.realtime;
+
+import com.karacatech.weatherforecast.CommonUtility;
+import com.karacatech.weatherforecast.GeolocationException;
+import com.karacatech.weatherforecast.GeolocationService;
+import com.karacatech.weatherforecast.common.Location;
+import com.karacatech.weatherforecast.common.RealtimeWeather;
+import com.karacatech.weatherforecast.location.LocationNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("api/v1/realtime")
+public class RealtimeWeatherApiController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RealtimeWeatherApiController.class);
+    private final GeolocationService geolocationService;
+    private final RealtimeWeatherService realtimeWeatherService;
+
+    @Autowired
+    public RealtimeWeatherApiController(GeolocationService geolocationService,
+                                        RealtimeWeatherService realtimeWeatherService) {
+        this.geolocationService = geolocationService;
+        this.realtimeWeatherService = realtimeWeatherService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getRealTimeWeatherByIPAddress(HttpServletRequest request) {
+        String ipAddress = CommonUtility.getIPAddress(request);
+
+        try {
+            Location locationFromIP = geolocationService.getLocation(ipAddress);
+            RealtimeWeather realtimeWeather = realtimeWeatherService.getByLocation(locationFromIP);
+
+            return ResponseEntity.ok(realtimeWeather);
+        } catch (GeolocationException e) {
+            LOGGER.error(e.getMessage(), e);
+
+            return ResponseEntity.badRequest().build();
+        } catch (LocationNotFoundException e) {
+            LOGGER.error(e.getMessage(), e);
+
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+}
