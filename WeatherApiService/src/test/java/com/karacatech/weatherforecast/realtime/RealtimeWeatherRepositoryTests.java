ba37eb4be0ef9070f8d4e963781ac5ec06915f8d
Bora <bora@Bora-MacBook-Air.local>
2023-04-17 12:54:45 +0300

    implement RealtimeWeatherRepository tests

---
+++ WeatherApiService/src/test/java/com/karacatech/weatherforecast/realtime/RealtimeWeatherRepositoryTests.java
@@ -0,0 +1,38 @@
+package com.karacatech.weatherforecast.realtime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.karacatech.weatherforecast.common.RealtimeWeather;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Date;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class RealtimeWeatherRepositoryTests {
+    @Autowired
+    private RealtimeWeatherRepository repository;
+
+    @Test
+    public void testUpdate() {
+        String locationCode = "BOSTON_US";
+        RealtimeWeather realtimeWeather = repository.findById(locationCode).get();
+
+        realtimeWeather.setTemperature(-2);
+        realtimeWeather.setHumidity(32);
+        realtimeWeather.setPrecipitation(43);
+        realtimeWeather.setStatus("Snowy");
+        realtimeWeather.setWindSpeed(12);
+        realtimeWeather.setLastUpdated(new Date());
+
+        RealtimeWeather updatedRealtimeWeather = repository.save(realtimeWeather);
+
+        assertThat(updatedRealtimeWeather.getHumidity()).isEqualTo(32);
+    }
+}
