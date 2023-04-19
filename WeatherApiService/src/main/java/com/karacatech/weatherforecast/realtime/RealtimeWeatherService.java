441b6d2132843c1007d4a723ab723d70d32b6bfc
Bora <bora@Bora-MacBook-Air.local>
2023-04-19 17:49:05 +0300

    implement update realtime weather data service

--- WeatherApiService/src/main/java/com/karacatech/weatherforecast/realtime/RealtimeWeatherService.java
+++ WeatherApiService/src/main/java/com/karacatech/weatherforecast/realtime/RealtimeWeatherService.java
@@ -3,18 +3,23 @@ package com.karacatech.weatherforecast.realtime;
 import com.karacatech.weatherforecast.common.Location;
 import com.karacatech.weatherforecast.common.RealtimeWeather;
 import com.karacatech.weatherforecast.location.LocationNotFoundException;
+import com.karacatech.weatherforecast.location.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Service
 public class RealtimeWeatherService {
     private final RealtimeWeatherRepository realtimeWeatherRepository;
+    private final LocationRepository locationRepository;
 
     @Autowired
-    public RealtimeWeatherService(RealtimeWeatherRepository realtimeWeatherRepository) {
+    public RealtimeWeatherService(RealtimeWeatherRepository realtimeWeatherRepository,
+                                  LocationRepository locationRepository) {
         this.realtimeWeatherRepository = realtimeWeatherRepository;
+        this.locationRepository = locationRepository;
     }
 
     public RealtimeWeather getByLocation(Location location) throws LocationNotFoundException {
@@ -31,5 +36,18 @@ public class RealtimeWeatherService {
                 .orElseThrow(() -> new LocationNotFoundException(
                         "Location not found with the given location code: " + locationCode)
                 );
+    }
+
+    public RealtimeWeather update(String locationCode, RealtimeWeather realtimeWeather) throws LocationNotFoundException {
+        Location location = locationRepository.findByCode(locationCode);
+
+        if (location == null) {
+            throw new LocationNotFoundException("Location not found with the given location code: " + locationCode);
+        }
+
+        realtimeWeather.setLocationCode(locationCode);
+        realtimeWeather.setLastUpdated(new Date());
+
+        return realtimeWeatherRepository.save(realtimeWeather);
     }
 }
